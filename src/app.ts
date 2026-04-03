73de6076f4bb665251c936eaf7a3c25d68a24c2b
Marcelo Eugênio <celoeug@gmail.com>
2026-04-03 17:57:08 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./docs/swagger.js";
+import { pipelineRouter } from "./routes/pipeline.js";
 import { uploadRouter } from "./routes/upload.js";
 
 const app = express();
@@ -7,7 +10,17 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(uploadRouter);
+app.use(pipelineRouter);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customSiteTitle: "API Docs",
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Swagger UI: http://localhost:${PORT}/docs`);
 });
